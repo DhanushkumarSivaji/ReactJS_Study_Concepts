7c041d4fe2f0c5ed887c4b4f447840b4bc50e85f
Dhanushkumar Sivaji <dhanushkumar.sivaji@gswvss.com>
2019-07-01 15:50:08 +0530

    Error Boundary

--- react_learn/src/App.js
+++ react_learn/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ClickCounter from './HigherOrderComponents/ClickCounter'
 import HoverCounter from './HigherOrderComponents/HoverCounter'
+import Hero from './ErrorBoundary/Hero'
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <ClickCounter name={"Dhanush"}/>
-     <HoverCounter name={"Dharma"}/>
+      {/*HOC*/}
+      {/* <ClickCounter name={"Dhanush"}/>
+      <HoverCounter name={"Dharma"}/> */}
+      {/*Error Boundary*/}
+      {/* <ErrorBoundary>
+      <Hero heroName="Dhanush"/>
+      </ErrorBoundary>
+      <ErrorBoundary>
+      <Hero heroName="Dharma"/>
+      </ErrorBoundary>
+      <ErrorBoundary>
+      <Hero heroName="Joker"/>
+      </ErrorBoundary> */}
     </div>
   );
 }
